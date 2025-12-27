2d30ec3b4e1e899b451175f25017a8eada8e71da
Sowmiya <sowmiyashan3@gmail.com>
2025-12-27 10:10:27 -0600

    Added spring boot projects for all the services and completed discover server module

---
+++ backend/inventory-service/src/test/java/com/walmart_clone/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart_clone.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
